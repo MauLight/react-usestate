ebfcf1851b7bee812b03d0373ca211fb79821929
MauLight <maulisseluz@gmail.com>
2023-03-25 12:15:35 -0300

    first exercise done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [restaurantName, setRestaurantName] = useState("Lemon");
+  console.log(restaurantName);
+
+  const updateName = () => {
+    setRestaurantName("Little Lemon")
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+    setRestaurantName(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{`Hello There ${restaurantName}!`}</h1>
+      <input type='text' value={restaurantName} onChange={handleChange} />
+      <button className='btn rounded -0 border' onClick={updateName}>Default</button>
     </div>
+
   );
 }
 
